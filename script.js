2e271e7dad6662f258ccffe10ee3449f154a7581
Thomas Hong <thomas_hong23@milton.edu>
2021-10-13 11:26:57 -0400

    messing around with js and font size stuff

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ const buttons = document.querySelectorAll('button');
 function changeColor(i) {
   document.getElementByClass(i).style.backgroundColor = 'red';
 }
+var time = null;
+function fontSizeNav() {
+  var elem = document.getElementById("unselectedspan");
+  var fsize = 20;
+  clearInterval(time);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (fsize >= 50) {
+      clearInterval(id);
+    }
+    else {
+      fsize++;
+      elem.style.fontSize = fsize + 'px';
+    }
+  }
+}
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myAnimation");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+
+    }
+  }
+}
